3421fcd1d1e05e6b477507a5148f0054b25a46ca
darinkagm <dariinkagm@gmail.com>
2021-02-06 13:30:15 +0100

    commit hw for week 3

---
+++ javascript/javascript1/week3/index.js
@@ -0,0 +1,94 @@
+//Item array removal :)
+
+const names = [
+    'Peter',
+    'Ahmad',
+    'Yana',
+    'kristina',
+    'Rasmus',
+    'Samuel',
+    'katrine',
+    'Tala',
+  ];
+  
+  names.splice(1, 1);
+  
+  console.log (names); 
+
+
+
+//When will we be there?
+  const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  }
+  ;
+  function trip () {
+  
+    var time = travelInformation.destinationDistance/travelInformation.speed;
+    var hours = parseInt(Number(time));
+    var minutes = Math.round((Number(time)- hours) *60 );
+    console.log ("The total duration of the trip is: " + hours+ " hours and "+ minutes + " minutes.")
+  }
+  const travelTime = trip();
+  console.log(travelTime); 
+
+
+
+
+  //Series :D
+  const seriesDurations = [
+    {
+      title: "Community",
+      totalInMinutes: 2420,
+    },
+    {
+      title: "The office",
+      totalInMinutes: 4290,
+    },
+     {
+      title: "The Handmaid's tale",
+      totalInMinutes: 1800,
+    },
+  ];
+  
+  var life = (42048000);
+  
+  function logOutSeriesText() {
+    for(let i= 0; i < seriesDurations.length; i++){
+    let percentage = (seriesDurations[i].totalInMinutes / life * 100);
+  console.log ("You've spent " + percentage + "%" + " of your life watching: " + (seriesDurations[i].title) + ".");
+  }
+  }
+  
+  logOutSeriesText()
+
+
+  
+  
+  
+  
+  
+  // NOnoN0nOYes (Note taking app)
+    const notes = [];
+  
+    function saveNote(content, id) {
+      var addTag = {"content": content, "id": id};
+     notes.push(addTag);
+    }
+    
+    saveNote("Call my brother", 1);
+    saveNote("Fix the bike", 2);
+    
+    console.log(notes);
+
+    function getNote(id) {
+        for (var i = 0; i < notes.length; i++){
+       if (id === notes[i].id) {
+         return notes[i];
+       }
+     }
+     }
+     const thirdNote = getNote(3);
+     console.log (thirdNote); 
+  
