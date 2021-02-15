400710294bf833a886028773e9597026ca197508
darinkagm <dariinkagm@gmail.com>
2021-02-15 22:05:19 +0100

    commit changes to week3 hw

--- javascript/javascript1/week3/index.js
+++ javascript/javascript1/week3/index.js
@@ -9,86 +9,86 @@ const names = [
     'Samuel',
     'katrine',
     'Tala',
-  ];
-  
-  names.splice(1, 1);
-  
-  console.log (names); 
+];
+const nameToRemove = "Ahmad";
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i] === nameToRemove) {
+        return names.splice(i, 1);
+    }
+}
+
+console.log(names);
 
 
 
 //When will we be there?
-  const travelInformation = {
+const travelInformation = {
     speed: 50,
     destinationDistance: 432,
-  }
-  ;
-  function trip () {
-  
-    var time = travelInformation.destinationDistance/travelInformation.speed;
+};
+
+function trip() {
+
+    var time = travelInformation.destinationDistance / travelInformation.speed;
     var hours = parseInt(Number(time));
-    var minutes = Math.round((Number(time)- hours) *60 );
-    console.log ("The total duration of the trip is: " + hours+ " hours and "+ minutes + " minutes.")
-  }
-  const travelTime = trip();
-  console.log(travelTime); 
+    var minutes = Math.round((Number(time) - hours) * 60);
+    console.log("The total duration of the trip is: " + hours + " hours and " + minutes + " minutes.")
+}
+const travelTime = trip();
+console.log(travelTime);
 
 
 
 
-  //Series :D
-  const seriesDurations = [
-    {
-      title: "Community",
-      totalInMinutes: 2420,
+//Series :D
+const seriesDurations = [{
+        title: "Community",
+        totalInMinutes: 2420,
     },
     {
-      title: "The office",
-      totalInMinutes: 4290,
+        title: "The office",
+        totalInMinutes: 4290,
     },
-     {
-      title: "The Handmaid's tale",
-      totalInMinutes: 1800,
+    {
+        title: "The Handmaid's tale",
+        totalInMinutes: 1800,
     },
-  ];
-  
-  var life = (42048000);
-  
-  function logOutSeriesText() {
-    for(let i= 0; i < seriesDurations.length; i++){
-    let percentage = (seriesDurations[i].totalInMinutes / life * 100);
-  console.log ("You've spent " + percentage + "%" + " of your life watching: " + (seriesDurations[i].title) + ".");
-  }
-  }
-  
-  logOutSeriesText()
+];
 
+var life = (42048000);
 
-  
-  
-  
-  
-  
-  // NOnoN0nOYes (Note taking app)
-    const notes = [];
-  
-    function saveNote(content, id) {
-      var addTag = {"content": content, "id": id};
-     notes.push(addTag);
+function logOutSeriesText() {
+    for (let i = 0; i < seriesDurations.length; i++) {
+        let percentage = (seriesDurations[i].totalInMinutes / life * 100);
+        console.log("You've spent " + percentage + "%" + " of your life watching: " + (seriesDurations[i].title) + ".");
     }
-    
-    saveNote("Call my brother", 1);
-    saveNote("Fix the bike", 2);
-    
-    console.log(notes);
+}
 
-    function getNote(id) {
-        for (var i = 0; i < notes.length; i++){
-       if (id === notes[i].id) {
-         return notes[i];
-       }
-     }
-     }
-     const thirdNote = getNote(3);
-     console.log (thirdNote); 
-  
+logOutSeriesText()
+
+
+
+
+// NOnoN0nOYes (Note taking app)
+const notes = [];
+
+function saveNote(task, id) {
+    const newNote = [("Content: " + task), ("id: " + id)]
+    notes.push(newNote);
+}
+
+saveNote("Call my brother", 1);
+saveNote("Fix the bike", 2);
+
+console.log(notes);
+
+function getNote(id) {
+    for (var i = 0; i < notes.length; i++) {
+        if (id === notes[i].id) {
+            return notes[i];
+        }
+    }
+}
+const thirdNote = getNote(3);
+console.log(thirdNote);
