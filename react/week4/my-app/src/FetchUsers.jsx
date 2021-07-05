d704e64e2336cbe11cf603c237701ead7b3d5128
darinkagm <dariinkagm@gmail.com>
2021-07-05 19:29:26 +0200

    feedback for react week4 implemented

--- react/week4/my-app/src/FetchUsers.jsx
+++ react/week4/my-app/src/FetchUsers.jsx
@@ -3,20 +3,20 @@ import Context from "./Context";
 
 function FetchUsers() {
   const users = React.useContext(Context);
-  const [displayData, fetchData] = useState([]);
+  const [userInput, setUserInput] = useState([]);
   useEffect(() => {
     if (!users) return;
     fetch(`https://api.github.com/search/users?q=${users}`)
       .then((res) => res.json())
-      .then((data) => fetchData(data.items))
+      .then((data) => setUserInput(data.items))
   }, [users]);
   if (!users) {
     return "Input error";
   }
-  if (!displayData) return "Error";
-  if (displayData.length === 0) return "No users found";
+  if (!userInput) return "Error";
+  if (userInput.length === 0) return "No users found";
   
-  return displayData.map((users) => <h3>{users.login}</h3>);
+  return userInput.map((users) => <h3>{users.login}</h3>);
 }
 
 export default FetchUsers;
